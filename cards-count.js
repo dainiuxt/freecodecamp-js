b066028b6503d73e992218ffcd0d63e2e69b44a1
Dainius <dainiuxt@users.noreply.github.com>
2020-04-15 19:10:42 +0300

    Create cards-count.js

---
+++ cards-count.js
@@ -0,0 +1,35 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/counting-cards
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch(card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count++
+    break;
+  case 7:
+  case 8:
+  case 9:
+    count = count;
+    break;
+  default:
+    count--;
+}
+if (count<0) {
+  return (count + " Hold");
+}
+else if (count > 0) {
+  return (count + " Bet");
+}
+else if (count === 0) {
+  return (count + " Hold");
+}
+  return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
